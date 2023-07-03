928146dcbc0938f1e87dc48e62197b63b1cf3bc3
bekloid <bekloid@gmail.com>
2023-07-04 00:34:16 +0300

    dr`n`dr

--- tests/test_drag_and_dropp.py
+++ tests/test_drag_and_dropp.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.droppable import Droppable
 from selenium.webdriver import ActionChains
 
@@ -6,6 +8,12 @@ def test_drag_and_drop(browser):
     drag_and_drop = Droppable(browser)
     drag_and_drop.visit()
     action_chains = ActionChains(browser)
-    assert action_chains.drop.check_css('backgroundColor', 'rgba(0, 0, 0, 0)')
-    action_chains.drag_and_drop_by_offset(drag_and_drop.find_element(), 700,468).perform()
-    assert action_chains.drop.check_css('backgroundColor', 'rgba(70, 130, 180, 1)')
+    assert drag_and_drop.drop.check_css('backgroundColor', 'rgba(0, 0, 0, 0)')
+    action_chains.drag_and_drop(drag_and_drop.drag.find_element(), drag_and_drop.drop.find_element()).perform()
+    time.sleep(2)
+    assert drag_and_drop.drop.check_css('backgroundColor', 'rgba(70, 130, 180, 1)')
+    time.sleep(2)
+
+    action_chains.drag_and_drop_by_offset(drag_and_drop.drag.find_element(), 700,468).perform()
+    time.sleep(3)
+    assert drag_and_drop.drop.check_css('backgroundColor', 'rgba(70, 130, 180, 1)')
